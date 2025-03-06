e80e7af774703425b9af0e6995181c83b00b13ae
JackWillett03 <jack-willett@outlook.com>
2025-03-06 21:51:32 +0000

    Testing cors

--- WebappDeploy/backend/server.js
+++ WebappDeploy/backend/server.js
@@ -13,7 +13,7 @@ const { Server } = require('socket.io');
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "*", 
+        origin: "https://shop-deployment-test-front.onrender.com", 
         methods: ["GET", "POST", "PUT", "DELETE"]
     }
 });
@@ -30,7 +30,7 @@ mongoose
 
 app.use(express.json());
 
-app.use(cors())
+app.use(cors(`https://shop-deployment-test-front.onrender.com`))
 
 app.use((req, res, next) => {
   req.io = io;
