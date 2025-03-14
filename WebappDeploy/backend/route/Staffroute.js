df115e3ae57164711f7cb326e2cc7f4472c2d25b
Jack Willett <jack-willett@outlook.com>
2025-03-14 19:43:22 +0000

    Now that the cloud DB has the owner account, made it so only Owners and managers can create staff

--- WebappDeploy/backend/route/Staffroute.js
+++ WebappDeploy/backend/route/Staffroute.js
@@ -7,9 +7,9 @@ router.get('/', Allowstaff({ allowOwner: true}), staffController.getStaff); // G
 router.get('/shop/:shopId', Allowstaff({ allowOwner: true, allowManager: true}), staffController.getStaffByShopId); // Get staff by Shopid
 router.get('/isManager/:isManager', Allowstaff({ allowOwner: true}), staffController.getStaffByIsManager); // Get staff based on if they're a manager
 router.get('/:id', staffController.getStaffById); // Get staff by Id
-router.post('/', staffController.addStaff); // Add new staff
+router.post('/', Allowstaff({ allowOwner: true, allowManager: true}), staffController.addStaff); // Add new staff
 router.post('/login', staffController.login); // Login
-router.put('/:id', staffController.updateStaff); // Update by Id
+router.put('/:id', Allowstaff({ allowOwner: true, allowManager: true, allowStaff: true}), staffController.updateStaff); // Update by Id
 router.delete('/:id', Allowstaff({ allowOwner: true, allowManager: true}), staffController.deleteStaff); // Delete staff by Id
 
 module.exports = router;
